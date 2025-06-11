cbb9515be52fcffc79add1fbd2dcb41ba69fd4b9
manar mustafa <abuhaitmanar@gmail.com>
2025-06-11 19:16:01 +0300

    new

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
-
+import logo from "../assets/logo.png"
 
 const NavBar = () => {
   return (
     <nav className="navbar">
         <div className="navBar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/" style={{display: "flex", alignItems: "center"}}><img src={logo} alt="logo" style={{marginRight: "4px", width: "30px", textAlign: "center"}}/> Movie App</Link>
         </div>
 
         <div className="navbar-links">
